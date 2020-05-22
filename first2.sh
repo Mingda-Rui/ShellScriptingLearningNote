26ae2b1c666e37234cc1c4cd988198de950e6592
Mingda Rui <mingdarui@gmail.com>
2020-05-22 18:46:41 +0100

    answers

--- first2.sh
+++ first2.sh
@@ -3,10 +3,11 @@
 echo "Hello     World"  # Hello    World
 echo "Hello World"      # Hello World
 echo "Hello * World"    # Hello * World
-echo Hello * World      #
+echo Hello * World      # Hello README.md first.sh first2.sh World
 echo Hello    World     # Hello World
 echo "Hello" World      # Hello World
 echo Hello "    " World # Hello      World
-echo "Heelo "*" World"  #
-echo `hello` World      #
-echo 'helo' world       #
+echo "Hello "*" World"  # Hello * World
+echo `hello` world      # ./first2.sh: line 11: hello: command not found
+                        # world
+echo 'hello' world      # hello world
