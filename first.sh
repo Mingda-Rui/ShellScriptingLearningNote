1d57401181f20860253b165f94a49577a1de9a60
Mingda Rui <mingdarui@gmail.com>
2020-05-22 18:39:49 +0100

    refactor

--- first.sh
+++ first.sh
@@ -1,12 +1,3 @@
 #!/bin/sh
 # This is a comment!
-echo "Hello     World"  # Hello    World
-echo "Hello World"      # Hello World
-echo "Hello * World"    # Hello * World
-echo Hello * World      #
-echo Hello    World     # Hello World
-echo "Hello" World      # Hello World
-echo Hello "    " World # Hello      World
-echo "Heelo "*" World"  #
-echo `hello` World      #
-echo 'helo' world       #
+echo "Hello    World" # This is a comment, too!
---
+++ first2.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+# This is a comment!
+echo "Hello     World"  # Hello    World
+echo "Hello World"      # Hello World
+echo "Hello * World"    # Hello * World
+echo Hello * World      #
+echo Hello    World     # Hello World
+echo "Hello" World      # Hello World
+echo Hello "    " World # Hello      World
+echo "Heelo "*" World"  #
+echo `hello` World      #
+echo 'helo' world       #
