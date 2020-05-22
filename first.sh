0536dce0d617f77e34d6d2b5649396b765e00d16
Mingda Rui <mingdarui@gmail.com>
2020-05-22 18:37:58 +0100

    echo hello world exercise with predicated output

--- first.sh
+++ first.sh
@@ -1,3 +1,12 @@
 #!/bin/sh
 # This is a comment!
-echo "Hello     World" # This is a comment, too!
+echo "Hello     World"  # Hello    World
+echo "Hello World"      # Hello World
+echo "Hello * World"    # Hello * World
+echo Hello * World      #
+echo Hello    World     # Hello World
+echo "Hello" World      # Hello World
+echo Hello "    " World # Hello      World
+echo "Heelo "*" World"  #
+echo `hello` World      #
+echo 'helo' world       #
