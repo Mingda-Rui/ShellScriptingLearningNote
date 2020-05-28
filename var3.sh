32b23c1f9e629108f6ce85ab3adfdfe3934e099f
Mingda Rui <mingdarui@gmail.com>
2020-05-28 19:22:03 +0100

    add example of basename and backtick

--- var3.sh
+++ var3.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 echo "I was called with $# parameters"
 echo "My name is $0"
+echo "My name is `basename $0`"
 echo "My first paramter is $1"
 echo "My second paramter is $2"
 echo "All parameters are $@"
