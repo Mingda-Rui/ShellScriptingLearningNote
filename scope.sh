7006c7d58a2d7f58ed3b292e314aeee3422954d8
Mingda Rui <mingdarui@gmail.com>
2020-05-29 15:14:30 +0100

    example of variable scope in function

---
+++ scope.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+myfunc()
+{
+  echo "I was called as : $@"
+  x=2
+}
+
+### Main script starts here
+
+echo "Script was called with $@"
+x=1
+echo "x is $x"
+myfunc 1 2 3
+myfunc 1 2 3 | tee out.log
+echo "x is $x"
